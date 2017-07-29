2c87561d5b901b23ca532518dce1017854b1a567
CrazyAlan <xiaochuan.chen91@gmail.com>
2017-07-29 00:34:45 -0400

    Finished pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/study/sfu', function(req, res, next) {
+  res.render('study/sfu.ejs', { title: 'sfu' });
+});
+
+
 module.exports = router;
