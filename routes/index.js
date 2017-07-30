c328e29f296546eca8d9d7f7213ded19162a2a80
CrazyAlan <xiaochuan.chen91@gmail.com>
2017-07-30 19:36:37 -0400

    Finished header and visa a

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var url = require('url')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/study/sfu', function(req, res, next) {
+  path = req.originalUrl
+  console.log(path.substr(1, path.length -1)+'.ejs')
   res.render('study/sfu.ejs', { title: 'sfu' });
+});
+
+router.get('/visa/*', function(req, res, next) {
+  path = req.originalUrl
+  pageName = path.substr(1, path.length -1)+'.ejs'
+  console.log(path.substr(1, path.length -1)+'.ejs')
+
+  res.render(pageName, { title: 'visa' });
 });
 
 
